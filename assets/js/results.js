d6cdd05e98bbd856eb3d2371ee0327deedcce20d
MadelineLowes <madeline.lowes@gmail.com>
2022-11-21 18:33:50 -0500

    continued to update javascript

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,6 +1,6 @@
-
 let showTitle = document.getElementById("titleId");
 let showPoster = document.getElementById("show-poster");
+let showLink = document.getElementById("show-link");
 let showGenres = document.getElementById("genresId");
 let showDuration = document.getElementById("durationId");
 let showReleaseDate = document.getElementById("releaseDateId");
@@ -9,28 +9,31 @@ let showUserRating = document.getElementById("userRatingId");
 let showCriticScore = document.getElementById("criticsScoreId");
 let showAvaileableOn = document.getElementById("availableOnId")
 let showType = document.getElementById("typeId");
-/* <li id="starringId">Starring:</li> */
+let similarTitle0 = document.getElementById("similarTitle0");
+let similarTitle1 = document.getElementById("similarTitle1");
+let similarTitle2 = document.getElementById("similarTitle2");
+let similarTitle3 = document.getElementById("similarTitle3");
+let titlePoster = [];
+
 
 function resultsPage() {
     // on load of second html, get object from local storage
     const showDetails = localStorage.getItem("searchedShow");
-    show = JSON.parse(showDetails)
+    show = JSON.parse(showDetails);
     // console.log("showDetails: ", JSON.parse(showDetails));
-    console.log(show);
-    console.log(show.title);
-    console.log(show.genres);
-    for (i = 0; i < show.genres.length; i++) {
-        console.log(show.genres[i]);
-    };
-    console.log(show.runtime);
-    console.log(show.releaseDate);
-    console.log(show.similarTitles);
-    console.log(show.userRating);
-    console.log(show.criticScore);
-    console.log(show.findOn);
-    console.log(show.type);
-
-
+    // console.log(show);
+    // console.log(show.title);
+    // console.log(show.genres);
+    // for (i = 0; i < show.genres.length; i++) {
+    //     console.log(show.genres[i]);
+    // };
+    // console.log(show.runtime);
+    // console.log(show.releaseDate);
+    // console.log(show.similarTitles);
+    // console.log(show.userRating);
+    // console.log(show.criticScore);
+    // console.log(show.findOn);
+    // console.log(show.type);
 
     showTitle.textContent = "Title: " + (show.title);
     showAvaileableOn.textContent = "Available on: " + (show.findOn);
@@ -41,29 +44,64 @@ function resultsPage() {
     showDuration.textContent = "Duration: " + (show.runtime);
     showGenres.textContent = "Genre(s): " + (show.genres);
     showType.textContent = "Type: " + (show.type);
-
-    showPoster = (show.poster);
+    showPoster.setAttribute('src', (show.poster));
+    // showLink.setAttribute('href', (show.link));
+    //this is the link from the poster
+    // href="https://www.disneyplus.com/en-gb/movies/encanto/33q7DY1rtHQH"
 
     // for this, we'll need to fetch the IDs and use those IDs to get the poster
-    // showSimilarTitles = (show.similarTitles);
-    for (i = 0; i < show.similarTitles.length; i++) {
-        // console.log(show.similarTitles[i]);
-        titleId = (show.similarTitles[i])
-        // then we can fetch using the id
-        idSearchLink = 'https://api.watchmode.com/v1/title/' + titleId + '/details/?apiKey=Fwh0ajztLiFlfUgwLyKvODRDtIlZpC46tMBPivKJ&append_to_response=sources';
-        // console.log(idSearchLink);
-        // can we do a pull in script.js for name, id & poster then store locally, so when we need to get the posters for similar titles, we dont need to call from api
+    for (i = 0; i < 4; i++) {
+        idTitle = (show.similarTitles[i])
+        idSearchLink = 'https://api.watchmode.com/v1/title/' + idTitle + '/details/?apiKey=8aHoTJyEcy2TVy9tyfBT3Nb0p8En8Fme0gzuLqfk&append_to_response=sources';
         fetch(idSearchLink)
             .then(function (response) {
                 return response.json();
             })
             .then(function (data) {
-                // console.log(data.title);
-                // console.log(data.poster);
-                showSimilarTitles = (data.poster)
+                titlePoster.push((data.poster));
+                console.log((data.poster));
+                console.log(typeof (data.poster));
+                console.log(titlePoster);
+                console.log(typeof titlePoster);
             });
+        console.log(i);
+        
+            if (i = 0){
+                let src = similarTitle0.getAttribute('src');
+                if (!src) {
+                    similarTitle0.setAttribute('src', titlePoster[0]);
+                }
+            } 
+            else if (i = 1) {
+                let src = similarTitle0.getAttribute('src');
+                if (!src) {
+                    similarTitle1.setAttribute('src', titlePoster[1]);
+                }
+            } else if (i = 2) {
+                let src = similarTitle0.getAttribute('src');
+                if (!src) {
+                    similarTitle2.setAttribute('src', titlePoster[2]);
+                }
+            } else if (i = 3) {
+                let src = similarTitle0.getAttribute('src');
+                if (!src) {
+                    similarTitle3.setAttribute('src', titlePoster[3]);
+                }
+            };
+        }
 
-    };
+    // }
+    // console.log(titlePoster);
+    // console.log(typeof titlePoster);
+    // console.log(titlePoster.keys());
+    // console.log(titlePoster.keys(titlePoster));
+
+
+    // console.log(titlePoster.at(0));
+
+    // console.log(titlePoster[0]);
+
+   
 };
 
 resultsPage();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-YOUR_API_KEY = "Fwh0ajztLiFlfUgwLyKvODRDtIlZpC46tMBPivKJ";
+YOUR_API_KEY = "8aHoTJyEcy2TVy9tyfBT3Nb0p8En8Fme0gzuLqfk";
 // const showInfo = document.querySelector("#show-info");
 // let count = document.getElementById("show-info").children().length;
 // const availableOn = showInfo.querySelector('#second');
@@ -6,17 +6,7 @@ YOUR_API_KEY = "Fwh0ajztLiFlfUgwLyKvODRDtIlZpC46tMBPivKJ";
 // const results = window.open("results.html")
 // let availableOn = results.document.getElementById("availableOnId")
 
-// fetch('https://api.watchmode.com/v1/sources/?apiKey=Fwh0ajztLiFlfUgwLyKvODRDtIlZpC46tMBPivKJ')
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//         console.log (data); 
-//         console.log(data[0].name);
-//         for (var i = 0; i < data.length; i++) {
-//             console.log(data[i].name);
-//         }
-//     });
+
 
 
 function addToURL() {
@@ -25,11 +15,10 @@ function addToURL() {
     console.log(searchValue);
     console.log(typeof searchValue);
 
-    let searchBarLink = 'https://api.watchmode.com/v1/search/?apiKey=Fwh0ajztLiFlfUgwLyKvODRDtIlZpC46tMBPivKJ&search_field=name&search_value='
+    let searchBarLink = 'https://api.watchmode.com/v1/search/?apiKey=8aHoTJyEcy2TVy9tyfBT3Nb0p8En8Fme0gzuLqfk&search_field=name&search_value='
 
     updatedSearchLink = searchBarLink + searchValue;
     console.log(updatedSearchLink);
-
 
     // then we'll fetch using the updated URL
     fetch(updatedSearchLink)
@@ -44,24 +33,24 @@ function addToURL() {
             let titleId = data.title_results[0].id;
 
             // then we can fetch using the id
-            idSearchLink = 'https://api.watchmode.com/v1/title/' + titleId + '/details/?apiKey=Fwh0ajztLiFlfUgwLyKvODRDtIlZpC46tMBPivKJ&append_to_response=sources';
-            console.log(idSearchLink);
+            idSearchLink = 'https://api.watchmode.com/v1/title/' + titleId + '/details/?apiKey=UNVpdPqhvDhdfT1BnzgUGbKpyx4zMHmYCbrGATUx&append_to_response=sources';
+            // console.log(idSearchLink);
             fetch(idSearchLink)
                 .then(function (response) {
                     return response.json();
                 })
                 .then(function (data) {
-                    console.log(data);
-                    console.log(data.title);
-                    console.log(data.runtime_minutes);
-                    console.log(data.poster);
-                    console.log(data.critic_score);
-                    console.log(data.genre_names);
-                    console.log(data.release_date);
-                    console.log(data.sources);
-                    console.log(data.similar_titles);
-                    console.log(data.type);
-                    console.log(data.user_rating);
+                    // console.log(data);
+                    // console.log(data.title);
+                    // console.log(data.runtime_minutes);
+                    // console.log(data.poster);
+                    // console.log(data.critic_score);
+                    // console.log(data.genre_names);
+                    // console.log(data.release_date);
+                    // console.log(data.sources);
+                    // console.log(data.similar_titles);
+                    // console.log(data.type);
+                    // console.log(data.user_rating);
 
                     for (i = 0; i < data.sources.length; i++) {
                         if (data.sources[i].type === "sub") {
@@ -89,18 +78,16 @@ function addToURL() {
                             // local storage if using a lot of data
                             localStorage.setItem("searchedShow", JSON.stringify(searchedShow));
                             window.location.replace("http://127.0.0.1:5502/results.html");
-
-                            
                         }
                     }
                 });
-         
+
         });
-    }
-        
+}
 
 
 
-        // function redirectResults(){
-        //     window.location.href='./results.html';
-        // };
+
+//         // function redirectResults(){
+//         //     window.location.href='./results.html';
+//         // };
