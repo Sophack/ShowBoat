e7314024a7200f9aecc1245a31f7225f8390022e
aponceha <114960304+aponceha@users.noreply.github.com>
2022-11-27 16:58:23 -0500

    Removed Maddie's Weather Code (interference)

--- assets/js/script.js
+++ assets/js/script.js
@@ -208,55 +208,55 @@ window.addEventListener("click", function (event) {
 
 
 //defined this variable outside the function
-let weatherApiKey = "594ea481b00a6604e497673b5c4fe941";
-//changed into a function instead of a variable
-//NEED to add code that determines which city user is in
-function weather() {
-    // fetchWeather: function (city) {
-    // make sure not to use "" but instead use `` when adding to a link that you want to change
-    // moved the url outside of the actual fetch and defined as a variable 
-    let detectedCity = "Toronto" //find a way to detect the user's location
-    let userCity = `https://api.openweathermap.org/data/2.5/weather?q=${detectedCity}&units=metric&appid=${weatherApiKey}`
+// let weatherApiKey = "594ea481b00a6604e497673b5c4fe941";
+// //changed into a function instead of a variable
+// //NEED to add code that determines which city user is in
+// function weather() {
+//     // fetchWeather: function (city) {
+//     // make sure not to use "" but instead use `` when adding to a link that you want to change
+//     // moved the url outside of the actual fetch and defined as a variable 
+//     let detectedCity = "Toronto" //find a way to detect the user's location
+//     let userCity = `https://api.openweathermap.org/data/2.5/weather?q=${detectedCity}&units=metric&appid=${weatherApiKey}`
 
-      fetch(userCity)        
-        .then(function (response) {
-          if (!response.ok) {
-            // alert("No weather found.");
-            // I don't think we're allowed to use alerts
-            throw new Error("No weather found.");
-            //end this function/provide error message
-          } else {
-          return response.json();
-          }
-        })
-        .then(function (data) {
-            // this.displayWeather(data);
-            displayWeather(data);
-        });
+//       fetch(userCity)        
+//         .then(function (response) {
+//           if (!response.ok) {
+//             // alert("No weather found.");
+//             // I don't think we're allowed to use alerts
+//             throw new Error("No weather found.");
+//             //end this function/provide error message
+//           } else {
+//           return response.json();
+//           }
+//         })
+//         .then(function (data) {
+//             // this.displayWeather(data);
+//             displayWeather(data);
+//         });
 
-        function displayWeather (data) {
-            let name = data;
-            // what is this? v
-            let { icon, description } = data.weather[0];
-            // we don't need these
-            // let { temp } = data.main;
-            // let { speed } = data.wind;
-            // innerText or innerHTML?
-            document.querySelector(".city").innerText = "Movie night in:  " + name;
-            document.querySelector(".icon").src =
-              "https://openweathermap.org/img/wn/" + icon + ".png";
-            document.querySelector(".weatherDescription").innerText = description;
-            // document.querySelector(".wind").innerText =
-            //   "Wind speed: " + speed + " km/h";
-            //changed classList.remove to removeClass
-            // document.querySelector(".weather").removeClass(".loading");
-          }
+//         function displayWeather (data) {
+//             let name = data;
+//             // what is this? v
+//             let { icon, description } = data.weather[0];
+//             // we don't need these
+//             // let { temp } = data.main;
+//             // let { speed } = data.wind;
+//             // innerText or innerHTML?
+//             document.querySelector(".city").innerText = "Movie night in:  " + name;
+//             document.querySelector(".icon").src =
+//               "https://openweathermap.org/img/wn/" + icon + ".png";
+//             document.querySelector(".weatherDescription").innerText = description;
+//             // document.querySelector(".wind").innerText =
+//             //   "Wind speed: " + speed + " km/h";
+//             //changed classList.remove to removeClass
+//             // document.querySelector(".weather").removeClass(".loading");
+//           }
 
 
     // search: function () {
     //   this.fetchWeather(document.querySelector(".search-bar").value);
     // },
-  };
+//   };
 
 
 //   document.querySelector(".search button").addEventListener("click", function () {
@@ -274,13 +274,13 @@ function weather() {
 //   weather.fetchWeather("Denver");
 
 // what's the event?
-  document.querySelector(".test").addEventListener("click", function (event) {
-      if (description == "Clear-Sky") {
-        console.log("not tonight!");
-      } else {
-        // ml: missing a whole section of code; if (description doesn't equal clear-sky) {tell user today is a good day to watch movie} else {present the next day that the weather isn't good}
-      }
-    });
+//   document.querySelector(".test").addEventListener("click", function (event) {
+//       if (description == "Clear-Sky") {
+//         console.log("not tonight!");
+//       } else {
+//         // ml: missing a whole section of code; if (description doesn't equal clear-sky) {tell user today is a good day to watch movie} else {present the next day that the weather isn't good}
+//       }
+//     });
 
 //below function is for the weather api
 function weather() {
